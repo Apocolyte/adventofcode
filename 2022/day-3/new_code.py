8cb7325cde8c674c979504126d64ba91a4942375
Apocolyte <benrvrv@gmail.com>
2022-12-03 18:09:44 +0000

    even more improvements

--- 2022/day-3/new_code.py
+++ 2022/day-3/new_code.py
@@ -10,20 +10,14 @@ class Code:
             second_compartment = rucksack[len(rucksack) // 2:]
 
             both_compartments_char = list(set(first_compartment).intersection(second_compartment))[0]
-
             all_values.append(Code.get_value[both_compartments_char])
 
         return sum(all_values)
 
     def part2(self):
-        grouped_rucksacks = []
-        for i in range(0, len(Code.data), 3):
-            grouped_rucksacks.append([Code.data[i], Code.data[i+1], Code.data[i+2]])
-
         all_values = []
-        for group in grouped_rucksacks:
-            all_elves_char = list(set(group[0]).intersection(group[1], group[2]))[0]
-             
+        for i in range(0, len(Code.data), 3):
+            all_elves_char = list(set(Code.data[i]).intersection(Code.data[i+1], Code.data[i+2]))[0]
             all_values.append(Code.get_value[all_elves_char])
 
         return sum(all_values)
