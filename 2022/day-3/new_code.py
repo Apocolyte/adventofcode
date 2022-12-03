7f89015c980f18873856bb7509148b968f714e91
Apocolyte <benrvrv@gmail.com>
2022-12-03 18:16:25 +0000

    even more improvements

--- 2022/day-3/new_code.py
+++ 2022/day-3/new_code.py
@@ -3,23 +3,23 @@ class Code:
     get_value = {char: index+1 for index, char in enumerate("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")}
 
     def part1(self):
-        all_values = []
+        total = 0
         for rucksack in Code.data:
             first_compartment = rucksack[:len(rucksack) // 2]
             second_compartment = rucksack[len(rucksack) // 2:]
 
             both_compartments_char = list(set(first_compartment) & set(second_compartment))[0]
-            all_values.append(Code.get_value[both_compartments_char])
+            total += Code.get_value[both_compartments_char]
 
-        return sum(all_values)
+        return total
 
     def part2(self):
-        all_values = []
+        total = 0
         for i in range(0, len(Code.data), 3):
             all_elves_char = list(set(Code.data[i]) & set(Code.data[i+1]) & set(Code.data[i+2]))[0]
-            all_values.append(Code.get_value[all_elves_char])
+            total += Code.get_value[all_elves_char]
 
-        return sum(all_values)
+        return total
 
 code = Code()
 print(code.part1())
