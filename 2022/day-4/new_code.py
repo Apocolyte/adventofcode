6a3694501fd461f29089aeeb74e07a5ae9af4e13
Apocolyte <benrvrv@gmail.com>
2022-12-05 04:22:47 +0000

    updated .gitignore

--- 2022/day-4/new_code.py
+++ 2022/day-4/new_code.py
@@ -1,18 +1,22 @@
-full_ranges_intermediate = [list(map(int, i.strip().replace(",", "-").split("-"))) for i in open("input.txt")]
-full_ranges = [[list(range(i[0],i[1])), list(range(i[2], i[3]))] for i in full_ranges_intermediate]
+data = [f.strip().split(",") for f in open("input.txt")]
+data_values = [[list(map(int, j.split("-"))) for j in i] for i in data]
 
 number = 0
-for i in full_ranges:
-    if sorted(i[0]) == sorted(list(set(i[0]) & set(i[1]))) or sorted(i[1]) == sorted(list(set(i[0]) & set(i[1]))):
+for pair in data_values:
+    if pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]:
+        number += 1
+    elif pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1]:
         number += 1
 
 # part 1
 print(number)
 
-number = 0
-for i in full_ranges:
-    if len(list(set(i[0]) & set(i[1]))) > 0:
-        number += 1
-
-# part 2
-print(number)
+for pair in data_values:
+    if pair[0][0] <= pair[1][1] and pair
+# number = 0
+# for i in full_ranges:
+#     if len(list(set(i[0]) & set(i[1]))) != 0:
+#         number += 1
+# 
+# # part 2
+# print(number)
